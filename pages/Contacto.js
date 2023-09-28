05485637bbed2f2ba930a7ab448194be1a1a95e9
Sala G10 <CSalaG10@uniboyaca.edu.co>
2023-09-27 19:38:05 -0500

    segundo

---
+++ pages/Contacto.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import 'react-native-gesture-handler';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>contacto</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
